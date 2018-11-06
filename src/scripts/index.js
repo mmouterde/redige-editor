7f21db5e1bb552b07a5dcf7461da7ef0a279f165
Martin Mouterde <martin.mouterde@gmail.com>
2018-11-06 21:25:21 +0100

    add the remove image handler

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,10 +5,8 @@ import {RdImageBlot} from './rd-image/rd-image';
 const quill = new Quill('#editor', {
     theme: 'snow',
 });
+window.quill = quill;
 
-Quill.debug(true);
-
-console.log(RdImageBlot);
 Quill.register(RdImageBlot);
 
 let a = false;
@@ -16,7 +14,7 @@ document.querySelector('body').addEventListener('click', function () {
     if (!a) {
         let range = quill.getSelection(true);
         quill.insertText(range.index, '\n', Quill.sources.USER);
-        quill.insertEmbed(range.index + 1, 'rd-image', true, Quill.sources.USER);
+        quill.insertEmbed(range.index + 1, 'rd-image', {coucou:3}, Quill.sources.USER);
         quill.setSelection(range.index + 2, Quill.sources.SILENT);
         a = true;
     }
